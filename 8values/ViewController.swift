eb2c19a5095d1b076cc0da95278e319e64e8092e
Eric Lieu <ericlieu118@gmail.com>
2020-02-03 19:49:52 -0500

    Initial Commit

---
+++ 8values/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  8values
+//
+//  Created by user on 2/3/20.
+//  Copyright © 2020 eric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
