c760072555ca51f62951cb9bdf00021d3c9abe8d
Eric Lieu <ericlieu118@gmail.com>
2020-02-14 15:57:53 -0500

    first

---
+++ 8values/SaveViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SaveViewController.swift
+//  8values
+//
+//  Created by user on 2/5/20.
+//  Copyright © 2020 eric. All rights reserved.
+//
+
+import UIKit
+
+class SaveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 8values/custombutton.swift
@@ -0,0 +1,21 @@
+//
+//  custombutton.swift
+//  8values
+//
+//  Created by user on 2/4/20.
+//  Copyright © 2020 eric. All rights reserved.
+//
+
+import UIKit
+
+class custombutton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
